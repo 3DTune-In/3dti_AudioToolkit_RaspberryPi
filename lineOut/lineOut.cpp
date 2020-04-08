0a5df7bf532bff6fb270dc255b5418d573e7fb44
gonzupi <gonzupi6@gmail.com>
2020-04-08 17:54:48 +0200

    trying simplification

--- lineOut/lineOut.cpp
+++ lineOut/lineOut.cpp
@@ -1,8 +1,8 @@
 /**
-* \class line_out implementation
+* \class line_out
 *
 * \brief Declaration of class line_out
-* \date	december 2019
+* \date	April 2020
 *
 * \authors: Gonzalo Alfonso Bueno Santana
 * \b Contact : gonzupi6@gmail.com
@@ -14,17 +14,13 @@
 * \b Project: 3DTI (3D-games for TUNing and lEarnINg about hearing aids) ||
 * \b Website: http://3d-tune-in.eu/
 *
-* \b Copyright: University of Malaga - 2019
-*
-* \b Licence: This copy of 3dti_AudioToolkit is licensed to you under the terms described in the 3DTI_AUDIOTOOLKIT_LICENSE file included in this distribution.
-*
-* \b Acknowledgement: This project has received funding from the European Union's Horizon 2020 research and innovation programme under grant agreement No 644051
+* \b Copyright: University of Malaga - 2020
 */
 
 /*! \file */
 
-#ifndef M_PI
-#define M_PI  (3.14159265)
+#ifndef NUMBER_PI
+#define NUMBER_PI  (3.14159265)
 #endif
 #include<vector> // for array, at()
 #include<tuple> // for get()
@@ -36,193 +32,82 @@ using namespace std;
 
 #define DEFAULT_SAMPLING_RATE 44100
 #define DEFAULT_TONE_FRECUENCY 440
-#define DEFAULT_RIGHT_PHASE 0
-#define DEFAULT_LEFT_PHASE 0
 
-namespace line_out{
+namespace line_out_namespace{
 	//Start the lineOut stream with default configuration
 	//A sin wave of 440Hz, with a 44100 size of SampleRate.
-	lineOut::lineOut() : stream(0), left_phase(DEFAULT_LEFT_PHASE), right_phase(DEFAULT_RIGHT_PHASE), iSampleRate(DEFAULT_SAMPLING_RATE), iLeftSenFrecuency(DEFAULT_TONE_FRECUENCY), iRightSenFrecuency(DEFAULT_TONE_FRECUENCY)
-	{
-	   /* initialise sinusoidal wavetable */
-	   for( int i=0; i<iSampleRate; i++ )
-	   {
-	     lineOutLeftData->push_back ((float) sin( ((double)i/(double)iSampleRate) *iLeftSenFrecuency* M_PI * 2.));
-	     lineOutRightData->push_back((float) sin( ((double)i/(double)iSampleRate) *iRightSenFrecuency*M_PI * 2.));
-	   }
-	}
+	CLineOut::CLineOut() : stream(0){}
 
-	//Start the lineOut stream with external data. Needs to specify the sample_rate too.
-	lineOut::lineOut(vector <float> * rightData, vector <float> * leftData, int sample_rate): stream(0), left_phase(DEFAULT_LEFT_PHASE), right_phase(DEFAULT_RIGHT_PHASE), iSampleRate(sample_rate), iLeftSenFrecuency(DEFAULT_TONE_FRECUENCY), iRightSenFrecuency(DEFAULT_TONE_FRECUENCY)
-	{
-	  lineOutLeftData = leftData;
-	  lineOutRightData = rightData;
-	}
+	bool CLineOut::setup(PaDeviceIndex __index, int __iBufferSize, int __iSampleRate):iSampleRate(__iSampleRate), iBufferSize(__iBufferSize), {
+		PaStreamParameters outputParameters;
+		outputParameters.device = index;
+		if (outputParameters.device == paNoDevice) return false;//device not found
+		const PaDeviceInfo* pInfo = Pa_GetDeviceInfo(index);
+		if (pInfo != 0) printf("Output device name: '%s'\r", pInfo->name);
 
-	//Start the lineOut stream with external data. Needs to specify the sample_rate too and let the user to configure right and left phases.
-	lineOut::lineOut(vector <float> * rightData, vector <float> * leftData, int sample_rate, int leftPhase, int rightPhase): stream(0), left_phase(leftPhase), right_phase(rightPhase), iSampleRate(sample_rate), iLeftSenFrecuency(DEFAULT_TONE_FRECUENCY), iRightSenFrecuency(DEFAULT_TONE_FRECUENCY)
-	{
-	  lineOutLeftData = leftData;
-	  lineOutRightData = rightData;
-	}
+		outputParameters.channelCount = 2;       /* stereo output */
+		outputParameters.sampleFormat = paFloat32; /* 32 bit floating point output */
+		outputParameters.suggestedLatency = Pa_GetDeviceInfo( outputParameters.device )->defaultLowOutputLatency;
+		outputParameters.hostApiSpecificStreamInfo = NULL;
 
-	//Reconfigure the sin wave using new parameters filling the complete buffer again.
-	//Can be used while running
-	bool lineOut::Sine(int sampleRate ,int LeftFrecuency, int RightFrecuency, int leftPhase, int rightPhase, PaStream*  Stream)
-	{
-	  stream=Stream;
-	  iSampleRate=sampleRate;
-	  iLeftSenFrecuency=LeftFrecuency;
-	  iRightSenFrecuency=RightFrecuency;
-	  left_phase= int (((leftPhase)% (360))%(sampleRate));
-	  right_phase=int (((rightPhase)% (360))%(sampleRate));
+		PaError err = Pa_OpenStream(
+															 &stream,
+															 NULL, /* no input */
+															 &outputParameters,
+															 __iSampleRate,
+															 __iBufferSize,
+															 paClipOff,      /* we won't output out of range samples so don't bother clipping them */
+															 &lineOut::paCallback,
+															 this            /* Using 'this' for userData so we can cast to lineOut* in paCallback method */
+															 );
 
-	    /* initialise sinusoidal wavetable */
-	    for( int i=0; i<iSampleRate; i++ )
-	    {
+		if (err != paNoError) return false;	 /* Failed to open stream to device !!! */
 
-		 lineOutLeftData->push_back( (float) sin( ((double)i/(double)iSampleRate) *iLeftSenFrecuency* M_PI * 2. ));
-		 lineOutRightData->push_back((float) sin( ((double)i/(double)iSampleRate) *iRightSenFrecuency*M_PI * 2.));
-	    }
-	    return true;
-	    //sprintf( message, "No Message" );
-	}
-
-	//Reconfigure the sin wave of the left speaker using new frecuency filling the complete buffer again.
-	//Can be used while running
-	bool lineOut::setFrecuencyLeft ( int frecLeft)
-	{
-	  if(frecLeft>0){
-	    iLeftSenFrecuency = frecLeft;
-	    lineOutLeftData->erase(lineOutLeftData->begin(), lineOutLeftData->end());
-	  for( int i=0; i<iSampleRate; i++ )
-	  {
-	    lineOutLeftData->push_back( (float) sin( ((double)i/(double)iSampleRate) *iLeftSenFrecuency* M_PI * 2. ));
-	  }
-	    return true;
-	  }else{
-	    return false;
-	  }
-	}
-
-	//Reconfigure the sin wave of the right speaker using new frecuency filling the complete buffer again.
-	//Can be used while running
-	bool lineOut::setFrecuencyRight( int frecRight)
-	{
-	  if(frecRight>0){
-	    iRightSenFrecuency = frecRight;
-	    lineOutRightData->erase(lineOutRightData->begin(), lineOutRightData->end());
-	  for( int i=0; i<iSampleRate; i++ )
-	  {
-	    lineOutRightData->push_back( (float) sin( ((double)i/(double)iSampleRate) *iRightSenFrecuency* M_PI * 2. ));
-	  }
-	    return true;
-	  }else{
-	    return false;
-	  }
-
-	}
-
-	//Reconfigure the sin wave using a new sampleRate filling the complete buffer again.
-	bool lineOut::setSampleRate ( int sampleRate)
-	{
-	  if(sampleRate>0){
-	    iSampleRate = sampleRate;
-	    lineOutRightData->erase(lineOutRightData->begin(), lineOutRightData->end());
-	    lineOutLeftData->erase( lineOutLeftData->begin() , lineOutLeftData->end());
-	    for( int i=0; i<iSampleRate; i++ )
-	    {
-	      lineOutLeftData->push_back(  (float) sin( ((double)i/(double)iSampleRate) *iLeftSenFrecuency * M_PI * 2. ));
-	      lineOutRightData->push_back( (float) sin( ((double)i/(double)iSampleRate) *iRightSenFrecuency* M_PI * 2. ));
-	    }
-	    return true;
-	  }else{
-	    return false;
-	  }
-	}
-
-	//Open an lineOut device and configure it to be used with a "bufferSize"
-	//User needs to execute this before "start" function.
-	bool lineOut::open(PaDeviceIndex index, int bufferSize)
-	{
-	    PaStreamParameters outputParameters;
-
-	    outputParameters.device = index;
-	    if (outputParameters.device == paNoDevice) {
-		 return false;
-	    }
-
-	    const PaDeviceInfo* pInfo = Pa_GetDeviceInfo(index);
-	    if (pInfo != 0)
-	    {
-		 printf("Output device name: '%s'\r", pInfo->name);
-	    }
-
-	    outputParameters.channelCount = 2;       /* stereo output */
-	    outputParameters.sampleFormat = paFloat32; /* 32 bit floating point output */
-	    outputParameters.suggestedLatency = Pa_GetDeviceInfo( outputParameters.device )->defaultLowOutputLatency;
-	    outputParameters.hostApiSpecificStreamInfo = NULL;
-
-	    PaError err = Pa_OpenStream(
-		 &stream,
-		 NULL, /* no input */
-		 &outputParameters,
-		 iSampleRate,
-		 bufferSize,
-		 paClipOff,      /* we won't output out of range samples so don't bother clipping them */
-		 &lineOut::paCallback,
-		 this            /* Using 'this' for userData so we can cast to lineOut* in paCallback method */
-		 );
-
-	    if (err != paNoError)
-	    {
-		 /* Failed to open stream to device !!! */
-		 return false;
-	    }
-
-	    err = Pa_SetStreamFinishedCallback( stream, &lineOut::paStreamFinished );
-
-	    if (err != paNoError)
-	    {
-		 Pa_CloseStream( stream );
-		 stream = 0;
-
-		 return false;
-	    }
-	    return true;
-	}
+		err = Pa_SetStreamFinishedCallback( stream, &lineOut::paStreamFinished );
+		if (err != paNoError)
+		{
+			  Pa_CloseStream( stream );
+			  stream = 0;
+	  		return false;
+		}
+		return true;
+	}//setup ends
 
 	//Close the lineOut device of this lineOut variable.
-	bool lineOut::close()
+	bool CLineOut::close()
 	{
-	    if (stream == 0)
-		 return false;
+	    if (stream == 0) return false;
 
 	    PaError err = Pa_CloseStream( stream );
 	    stream = 0;
-
 	    return (err == paNoError);
 	}
 
 	//Start the lineOut streaming.
-	bool lineOut::start()
+	bool CLineOut::start()
 	{
-	    if (stream == 0)
-		 return false;
-
+	    if (stream == 0) return false;
 	    PaError err = Pa_StartStream( stream );
-
 	    return (err == paNoError);
 	}
 
 	//Stop the lineOut streaming.
-	bool lineOut::stop()
+	bool CLineOut::stop()
 	{
-	    if (stream == 0)
-		 return false;
-
+	    if (stream == 0) return false;
 	    PaError err = Pa_StopStream( stream );
-
 	    return (err == paNoError);
 	}
-}
+
+	bool CLineOut::autoTest(){
+    for( int i=0; i<iSampleRate; i++ )
+    {
+        (audioLeftData).push_back( (float) sin( ((double)i/(double)iSampleRate) *DEFAULT_TONE_FRECUENCY* M_PI * 2. ));
+        (audioRightData).push_back(audioLeftData[i]);
+    }
+		CLineAudio::start();
+		Pa_Sleep( NUM_SECONDS * 1000 );
+		CLineAudio::stop();
+		CLineAudio::close();
+	}
+}//CLineOut ends
--- lineOut/lineOut.hpp
+++ lineOut/lineOut.hpp
@@ -2,7 +2,7 @@
 * \class line_out
 *
 * \brief Declaration of class line_out
-* \date	december 2019
+* \date	April 2020
 *
 * \authors: Gonzalo Alfonso Bueno Santana
 * \b Contact : gonzupi6@gmail.com
@@ -14,11 +14,7 @@
 * \b Project: 3DTI (3D-games for TUNing and lEarnINg about hearing aids) ||
 * \b Website: http://3d-tune-in.eu/
 *
-* \b Copyright: University of Malaga - 2019
-*
-* \b Licence: This copy of 3dti_AudioToolkit is licensed to you under the terms described in the 3DTI_AUDIOTOOLKIT_LICENSE file included in this distribution.
-*
-* \b Acknowledgement: This project has received funding from the European Union's Horizon 2020 research and innovation programme under grant agreement No 644051
+* \b Copyright: University of Malaga - 2020
 */
 
 /*! \file */
@@ -32,139 +28,90 @@
 
 using namespace std;
 
-namespace line_out{
-	class lineOut
+namespace line_out_namespace{
+	class CLineOut
 		{
 		public:
 			//Start the lineOut stream with default configuration
 			//A sin wave of 440Hz, with a 44100 size of SampleRate.
-			lineOut();
-
-			//Start the lineOut stream with external data. Needs to specify the sample_rate too.
-			lineOut(vector <float> * rightData, vector <float> * leftData, int sample_rate);
-
-			//Start the lineOut stream with external data. Needs to specify the sample_rate too and let the user to configure right and left phases.
-			lineOut(vector <float> * rightData, vector <float> * leftData, int sample_rate, int leftPhase, int rightPhase);
-
-			//Reconfigure the sin wave using new parameters filling the complete buffer again.
-			//Can be used while running
-	    		bool Sine(int sampleRate ,int LeftFrecuency, int RightFrecuency, int leftPhase, int rightPhase, PaStream*  Stream);
-
-			//Reconfigure the sin wave of the left speaker using new frecuency filling the complete buffer again.
-			//Can be used while running
-	   		bool setFrecuencyLeft ( int frecLeft);
-
-			//Reconfigure the sin wave of the right speaker using new frecuency filling the complete buffer again.
-			//Can be used while running
-	    		bool setFrecuencyRight( int frecRight);
-
-			//Reconfigure the sin wave using a new sampleRate filling the complete buffer again.
-			bool setSampleRate ( int sampleRate);
-
+			CLineOut();
 			//Open an lineOut device and configure it to be used with a "bufferSize"
-			//User needs to execute this before "start" function.
-	    		bool open(PaDeviceIndex index, int bufferSize);
-
+			//User needs to execute this before "start" function
+      bool setup(PaDeviceIndex __index, int __iBufferSize, int __iSampleRate);
 			//Close the lineOut device of this lineOut variable.
-	   		bool close();
+	   	bool close();
 
 			//Start the lineOut streaming.
-	  		bool start();
-
+	  	bool start();
 			//Stop the lineOut streaming.
 	 		bool stop();
 
+			//play a #A4 440Hz for one second
+			bool autoTest();
 	private:
-	    /* The instance callback, where we have access to every method/variable in object of class Sine */
-	    int iSampleRate;
+	    int iSampleRate, iBufferSize;
 	    int iLeftSenFrecuency;
 	    int iRightSenFrecuency;
 	    PaStream *stream;
 	    vector <float> *lineOutLeftData;
 	    vector <float> *lineOutRightData;
-	    //float sine[];//iSampleRate];
-	    int left_phase;
-	    int right_phase;
-	    char message[20];
-	    
+	    char caMessage[20];
+			int ileftPhase= 0;
+			int iRightPhase=0;
+
 	    int paCallbackMethod(const void *inputBuffer, void *outputBuffer,
-		 unsigned long framesPerBuffer,
-		 const PaStreamCallbackTimeInfo* timeInfo,
-		 PaStreamCallbackFlags statusFlags)
-	    {
-		 float *out = (float*)outputBuffer;
-		 unsigned long i;
-
-		 (void) timeInfo; /* Prevent unused variable warnings. */
-		 (void) statusFlags;
-		 (void) inputBuffer;
-
-		 for( i=0; i<framesPerBuffer; i++ )
-		 {
-		     *out++ = lineOutLeftData->at(left_phase);  /* left */
-		     *out++ = lineOutRightData->at(right_phase);  /* right */
-		     left_phase += 1;
-		     if( left_phase >= iSampleRate ) left_phase -= iSampleRate;
-		     right_phase += 1;
-		     if( right_phase >= iSampleRate ) right_phase -= iSampleRate;
-		 }
-
-		 return paContinue;
-
-	    }
+					 unsigned long framesPerBuffer,
+					 const PaStreamCallbackTimeInfo* timeInfo,
+					 PaStreamCallbackFlags statusFlags)
+				    {
+							 float *out = (float*)outputBuffer;
+							 unsigned long i;
+							 (void) timeInfo; /* Prevent unused variable warnings. */
+							 (void) statusFlags;
+							 (void) inputBuffer;
+							 for( i=0; i<framesPerBuffer; i++ )
+							 {
+							     *out++ = lineOutLeftData->at(ileftPhase);  /* left */
+							     *out++ = lineOutRightData->at(iRightPhase);  /* right */
+							     ileftPhase += 1;
+							     if( ileftPhase >= iSampleRate ) ileftPhase -= iSampleRate;
+							     iRightPhase += 1;
+							     if( iRightPhase >= iSampleRate ) iRightPhase -= iSampleRate;
+							 }
+							 return paContinue;
+	    }//callback ends
 
 	    /* This routine will be called by the PortlineOut engine when lineOut is needed.
 	    ** It may called at interrupt level on some machines so don't do anything
 	    ** that could mess up the system like calling malloc() or free().
 	    */
 	    static int paCallback( const void *inputBuffer, void *outputBuffer,
-		 unsigned long framesPerBuffer,
-		 const PaStreamCallbackTimeInfo* timeInfo,
-		 PaStreamCallbackFlags statusFlags,
-		 void *userData )
+														 unsigned long framesPerBuffer,
+														 const PaStreamCallbackTimeInfo* timeInfo,
+														 PaStreamCallbackFlags statusFlags,
+														 void *userData )
 	    {
-		 /* Here we cast userData to Sine* type so we can call the instance method paCallbackMethod, we can do that since
-		    we called Pa_OpenStream with 'this' for userData */
-		 return ((lineOut*)userData)->paCallbackMethod(inputBuffer, outputBuffer,
-		     framesPerBuffer,
-		     timeInfo,
-		     statusFlags);
+					 /* Here we cast userData to Sine* type so we can call the instance method paCallbackMethod, we can do that since
+					    we called Pa_OpenStream with 'this' for userData */
+					 return ((lineOut*)userData)->paCallbackMethod(inputBuffer, outputBuffer,
+																					     framesPerBuffer,
+																					     timeInfo,
+																					     statusFlags);
 	    }
 
 
 	    void paStreamFinishedMethod()
 	    {
-		 printf( "Stream Completed: %s\n", message );
+		 		printf( "Stream Completed: %s\n", caMessage );
 	    }
 	    /*
 	     * This routine is called by portlineOut when playback is done.
 	     */
 	    static void paStreamFinished(void* userData)
 	    {
-		 return ((lineOut*)userData)->paStreamFinishedMethod();
+		 		return ((lineOut*)userData)->paStreamFinishedMethod();
 	    }
-	    
-	};
 
-	class ScopedPaHandler
-	{
-		public:
-		    ScopedPaHandler()
-			 : _result(Pa_Initialize())
-		    {
-		    }
-		    ~ScopedPaHandler()
-		    {
-			 if (_result == paNoError)
-			 {
-			     Pa_Terminate();
-			 }
-		    }
-
-		    PaError result() const { return _result; }
-
-		private:
-		    PaError _result;
-	};
-}
+	};//CLineOut ends
+}//namespace ends
 #endif
